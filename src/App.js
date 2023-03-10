584496aedaf6672daf7d4448162b65c6156cab5c
saikumar <saikumar@digisprint.com>
2023-03-10 11:56:14 +0530

    fixalert

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
         setTimeout(() => {
               setAlert(null)
-        } , 1500)
+        } , 10000)
 
         
   }
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
+//
 function Alert(props) {
   return (
-
-   props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-  <strong>{props.alert.type}</strong> :{props.alert.message}
-</div>
-
-    
+    <>
+    {
+      props.alert && (
+        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+            <strong>{props.alert.type}</strong> :{props.alert.message}
+        </div>
+      )
+    }
+  </>
   )
 }
 
